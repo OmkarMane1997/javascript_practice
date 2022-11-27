af206a9603c43245a598adb859b394645714e24e
omkarMane1997 <omii0.mane@gmail.com>
2022-11-27 15:46:53 +0530

    done

--- Array2.js
+++ Array2.js
@@ -42,3 +42,46 @@ let modify =shoppingCart.findIndex((n)=>{
 console.log('modify Tea to Green Tea',shoppingCart)
 
 
+// In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+let countries =['india','Cambodia', 'Timor-Lester', 'Indonesia', 'Laos', 'Malaysia',  'Philippines', 'Singapore', 'Thailand' , 'Vietnam','Ethiopia'];
+let nameCountries = "Ethiopia";
+let findCount= countries.findIndex((params)=>{
+   return  params.includes(nameCountries)
+})
+
+console.log('Countries index',  findCount)
+
+if (findCount > 0) {
+    console.log("print in upper case:-",countries[findCount].toUpperCase())
+} else {
+    countries.push(nameCountries)
+}
+
+console.log('update countries:-', countries)
+
+//In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+let webTechs =['HTML', 'CSS','JS', 'React', 'Redux'];
+
+
+let findWeb = webTechs.findIndex((skills)=>{
+   return skills.includes('Sass')
+})
+
+if (findWeb>0) {
+    console.log(`${webTechs[findWeb]} is a CSS preprocess`)
+} else {
+    webTechs.push('Sass')
+    console.log('update array:-',webTechs )
+}
+
+
+// Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+
+const fullStack = [...frontEnd,...backEnd]
+
+console.log('Fullstack array:-',fullStack)
